ddf2e8a0f56d9d4f8942c6b4674a56dcb3a3d77d
Ilia Godlewski <godleevsky2015@gmail.com>
2025-08-12 18:48:10 +0200

    some fixes

--- src/Pathfinding.App.Console/Extensions/MessengerExtensions.cs
+++ src/Pathfinding.App.Console/Extensions/MessengerExtensions.cs
@@ -7,30 +7,14 @@ namespace Pathfinding.App.Console.Extensions;
 
 internal static class MessengerExtensions
 {
-    private sealed class UnregisterTokenizedHandler<TMessage, TToken>(
-        IMessenger messenger, object recipient, TToken token) : IDisposable
-        where TMessage : class
-        where TToken : IEquatable<TToken>
+    private sealed class UnsubcribeHandler(Action handler) : IDisposable
     {
-        public void Dispose()
-        {
-            messenger.Unregister<TMessage, TToken>(recipient, token);
-        }
-    }
-
-    private sealed class UnregisterHandler<TMessage>(
-        IMessenger messenger, object recipient) : IDisposable
-        where TMessage : class
-    {
-        public void Dispose()
-        {
-            messenger.Unregister<TMessage>(recipient);
-        }
+        public void Dispose() => handler?.Invoke();
     }
 
     public static IDisposable RegisterAwaitHandler<TMessage, TToken>(this IMessenger messenger,
         object recipient, TToken token, Func<object, TMessage, Task> handler)
-        where TMessage : class, IAwaitMessage
+        where TMessage : class, IAwaitableMessage
         where TToken : IEquatable<TToken>
     {
         messenger.Register<TMessage, TToken>(recipient, token, 
@@ -40,7 +24,8 @@ internal static class MessengerExtensions
                 msg.SetCompleted();
             });
 
-        return new UnregisterTokenizedHandler<TMessage, TToken>(messenger, recipient, token);
+        return new UnsubcribeHandler(() 
+            => messenger.Unregister<TMessage, TToken>(recipient, token));
     }
 
     public static IDisposable RegisterHandler<TMessage>(this IMessenger messenger, 
@@ -48,7 +33,8 @@ internal static class MessengerExtensions
         where TMessage : class
     {
         messenger.Register(recipient, action);
-        return new UnregisterHandler<TMessage>(messenger, recipient);
+        return new UnsubcribeHandler(() 
+            => messenger.Unregister<TMessage>(recipient));
     }
 
     public static IDisposable RegisterHandler<TMessage, TToken>(this IMessenger messenger,
@@ -57,7 +43,8 @@ internal static class MessengerExtensions
         where TToken : IEquatable<TToken>
     {
         messenger.Register<TMessage, TToken>(recipient, token, action);
-        return new UnregisterTokenizedHandler<TMessage, TToken>(messenger, recipient, token);
+        return new UnsubcribeHandler(()
+            => messenger.Unregister<TMessage, TToken>(recipient, token));
     }
 
     public static IDisposable RegisterAsyncHandler<TMessage>(this IMessenger messenger,
@@ -66,6 +53,7 @@ internal static class MessengerExtensions
     {
         messenger.Register<TMessage>(recipient, async (r, msg) 
             => await handler(r, msg).ConfigureAwait(false));
-        return new UnregisterHandler<TMessage>(messenger, recipient);
+        return new UnsubcribeHandler(() 
+            => messenger.Unregister<TMessage>(recipient));
     }
 }
