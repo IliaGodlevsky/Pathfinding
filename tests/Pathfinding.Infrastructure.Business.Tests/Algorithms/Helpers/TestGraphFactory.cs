c7865275fd5a5e03c26674d670eba442953088f3
Ilya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2025-11-09 15:20:11 +0100

    Reduce obstacles to ten percent coverage

--- tests/Pathfinding.Infrastructure.Business.Tests/Algorithms/Helpers/TestGraphFactory.cs
+++ tests/Pathfinding.Infrastructure.Business.Tests/Algorithms/Helpers/TestGraphFactory.cs
@@ -34,15 +34,15 @@ internal static class TestGraphFactory
 
     private static readonly bool[,] ObstacleMatrix = new bool[GridSize, GridSize]
     {
-        { false, true,  false, true,  false, true,  false, true,  false, true  },
-        { false, true,  true,  false, true,  false, true,  true,  false, true  },
-        { false, true,  false, true,  true,  false, true,  false, true,  true  },
-        { false, true,  true,  true,  false, true,  false, true,  false, true  },
-        { false, true,  false, false, true,  true,  true,  false, true,  true  },
-        { false, true,  true,  false, true,  false, true,  true,  false, true  },
-        { false, true,  false, true,  false, true,  true,  false, true,  true  },
-        { false, true,  true,  false, true,  true,  false, true,  false, true  },
-        { false, true,  true,  true,  true,  true,  true,  true,  true,  true  },
+        { false, false, false, true,  false, false, false, false, false, false },
+        { false, false, false, false, true,  false, false, true,  false, false },
+        { false, false, false, false, false, false, true,  false, false, false },
+        { false, false, true,  false, false, false, false, false, false, false },
+        { false, false, false, false, false, true,  false, false, false, false },
+        { false, false, false, false, false, false, false, false, true,  false },
+        { false, true,  false, false, false, false, false, false, false, false },
+        { false, false, false, false, false, false, false, true,  false, false },
+        { false, false, false, false, true,  false, false, false, false, false },
         { false, false, false, false, false, false, false, false, false, false },
     };
 
